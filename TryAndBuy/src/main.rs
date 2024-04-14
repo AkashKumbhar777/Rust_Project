75d16c37b6027587cc910e9dd35f1705105d9274
connecttusharp <164457247+connecttusharp@users.noreply.github.com>
2024-04-14 16:11:54 +0530

    commoit

--- TryAndBuy/src/main.rs
+++ TryAndBuy/src/main.rs
@@ -34,23 +34,26 @@ async fn main() -> std::io::Result<()> {
             .service(web::resource("/authenticate").route(web::post().to(authenticate)))
             .service(web::resource("/admin/products").route(web::get().to(controller::admin::get_products)))
             .service(web::resource("/admin/product").route(web::post().to(controller::admin::create_product)))
-            .service(web::resource("/user").route(web::post().to(controller::user_handler::create_user))) //User HAndlers
+            .service(web::resource("/user").route(web::post().to(controller::user_handler::create_user))) 
             .service(web::resource("/users").route(web::get().to(controller::user_handler::get_users)))
             .service(web::resource("/user/{id}").route(web::get().to(controller::user_handler::get_user_by_id)))
             .service(web::resource("/user/update/{id}").route(web::put().to(controller::user_handler::update_user)))
             .service(web::resource("/user/delete/{id}").route(web::delete().to(controller::user_handler::delete_user)))
 
-            .service(web::resource("/trycart/{uid}").route(web::post().to(controller::trycart_handler::create_trycart)))//TryCart Handlers
-            .service(web::resource("/getAlltrycarts").route(web::get().to(controller::trycart_handler::get_all_trycarts)))
-            .service(web::resource("/trycarts/{uid}").route(web::get().to(controller::trycart_handler::get_trycart_by_user_id)))
+            .service(web::resource("/trycart/{uid}").route(web::post().to(controller::trycart_handler::create_trycart)))
+            .service(web::resource("/trycarts").route(web::get().to(controller::trycart_handler::get_all_trycarts))) 
+            .service(web::resource("/trycart/{uid}").route(web::get().to(controller::trycart_handler::get_trycart_by_user_id)))
             .service(web::resource("/trycart/update/{uid}/{id}").route(web::put().to(controller::trycart_handler::update_trycart)))
             .service(web::resource("/trycart/delete/{uid}/{id}").route(web::delete().to(controller::trycart_handler::delete_trycart)))
-
-            .service(web::resource("/buycart/{uid}").route(web::post().to(controller::buycart_handler::create_buycart)))//BuyCart Handlers
+            
+            .service(web::resource("/buycart/{uid}").route(web::post().to(controller::buycart_handler::create_buycart)))
             .service(web::resource("/buycarts").route(web::get().to(controller::buycart_handler::get_all_buycarts)))
-            .service(web::resource("/buycarts/{uid}").route(web::get().to(controller::buycart_handler::get_buycart_by_user_id)))
+            .service(web::resource("/buycart/{uid}").route(web::get().to(controller::buycart_handler::get_buycart_by_user_id))) 
             .service(web::resource("/buycart/update/{uid}/{id}").route(web::put().to(controller::buycart_handler::update_buycart)))
             .service(web::resource("/buycart/delete/{uid}/{id}").route(web::delete().to(controller::buycart_handler::delete_buycart)))
+            
+
+
     })
     .bind("127.0.0.1:3000")?
     .run()
