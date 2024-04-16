f5629ca1c1d4a5c6bb9a2d479cb164ed06e6d91e
Akash Kumbhar <164382097+AkashKumbhar777@users.noreply.github.com>
2024-04-16 11:25:27 +0530

    commit

--- TryAndBuy/target/debug/deps/TryAndBuy-aaea3adb7e94d5da.d
+++ TryAndBuy/target/debug/deps/TryAndBuy-aaea3adb7e94d5da.d
@@ -1,6 +1,6 @@
-/Users/apple/Rust_Project/TryAndBuy/target/debug/deps/libTryAndBuy-aaea3adb7e94d5da.rmeta: src/main.rs src/controller/mod.rs src/controller/admin.rs src/controller/auth.rs src/controller/user_handler.rs src/controller/trycart_handler.rs src/controller/buycart_handler.rs src/controller/address_handler.rs src/db_connect/mod.rs src/db_connect/db.rs src/model/mod.rs src/model/login.rs src/model/user.rs src/model/product.rs src/model/address.rs src/model/buy_cart.rs src/model/order.rs src/model/refund_return.rs src/model/sales_history.rs src/model/try_cart.rs
+/Users/apple/Rust_Project/TryAndBuy/target/debug/deps/libTryAndBuy-aaea3adb7e94d5da.rmeta: src/main.rs src/controller/mod.rs src/controller/admin.rs src/controller/auth.rs src/controller/user_handler.rs src/controller/trycart_handler.rs src/controller/buycart_handler.rs src/controller/address_handler.rs src/controller/order_handler.rs src/db_connect/mod.rs src/db_connect/db.rs src/model/mod.rs src/model/login.rs src/model/user.rs src/model/product.rs src/model/address.rs src/model/buy_cart.rs src/model/order.rs src/model/refund_return.rs src/model/sales_history.rs src/model/try_cart.rs
 
-/Users/apple/Rust_Project/TryAndBuy/target/debug/deps/TryAndBuy-aaea3adb7e94d5da.d: src/main.rs src/controller/mod.rs src/controller/admin.rs src/controller/auth.rs src/controller/user_handler.rs src/controller/trycart_handler.rs src/controller/buycart_handler.rs src/controller/address_handler.rs src/db_connect/mod.rs src/db_connect/db.rs src/model/mod.rs src/model/login.rs src/model/user.rs src/model/product.rs src/model/address.rs src/model/buy_cart.rs src/model/order.rs src/model/refund_return.rs src/model/sales_history.rs src/model/try_cart.rs
+/Users/apple/Rust_Project/TryAndBuy/target/debug/deps/TryAndBuy-aaea3adb7e94d5da.d: src/main.rs src/controller/mod.rs src/controller/admin.rs src/controller/auth.rs src/controller/user_handler.rs src/controller/trycart_handler.rs src/controller/buycart_handler.rs src/controller/address_handler.rs src/controller/order_handler.rs src/db_connect/mod.rs src/db_connect/db.rs src/model/mod.rs src/model/login.rs src/model/user.rs src/model/product.rs src/model/address.rs src/model/buy_cart.rs src/model/order.rs src/model/refund_return.rs src/model/sales_history.rs src/model/try_cart.rs
 
 src/main.rs:
 src/controller/mod.rs:
@@ -10,6 +10,7 @@ src/controller/user_handler.rs:
 src/controller/trycart_handler.rs:
 src/controller/buycart_handler.rs:
 src/controller/address_handler.rs:
+src/controller/order_handler.rs:
 src/db_connect/mod.rs:
 src/db_connect/db.rs:
 src/model/mod.rs:
